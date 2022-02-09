a2b490292f38980812dbf13bf3065c0a58530b16
Daniel Lord <daniellord75@gmail.com>
2022-02-09 16:08:43 -0500

    Week 3 Workshop admin auth

--- authenticate.js
+++ authenticate.js
@@ -39,3 +39,15 @@ exports.jwtPassport = passport.use(
 );
 
 exports.verifyUser = passport.authenticate('jwt', { session: false });
+
+exports.verifyAdmin = function (req, res, next) {
+    console.log('verifying admin status...');
+    if (req.user.admin) {
+        console.log('admin status verified');
+        next();
+    } else {
+        err = new Error('You are not authorized to perform this operation!');
+        err.status = 403;
+        return next(err);
+    }
+};
