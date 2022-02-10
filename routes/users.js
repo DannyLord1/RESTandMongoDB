7a96ad553322698893683d69f75ce12a774198dc
Daniel Lord <daniellord75@gmail.com>
2022-02-09 22:55:33 -0500

    cors for cross origin requests to express routers

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,13 @@ const express = require('express');
 const User = require('../models/user');
 const passport = require('passport');
 const authenticate = require('../authenticate');
+const cors = require('./cors');
 
 const router = express.Router();
 
 /* GET users listing. */
 router.route('/')
-.get((authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+.get((cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     User.find()
     .then(users => {
         res.statusCode = 200;
@@ -17,7 +18,7 @@ router.route('/')
     .catch(err => next(err));
  }));
 
-router.post('/signup', (req, res) => {
+router.post('/signup', cors.corsWithOptions,  (req, res) => {
     User.register(
         new User({ username: req.body.username }),
         req.body.password,
@@ -51,7 +52,7 @@ router.post('/signup', (req, res) => {
     );
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', cors.corsWithOptions,  passport.authenticate('local'), (req, res) => {
     const token = authenticate.getToken({ _id: req.user._id });
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
@@ -59,7 +60,7 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
 });
 
 
-router.get('/logout', (req, res, next) => {
+router.get('/logout', cors.corsWithOptions,  (req, res, next) => {
     if (req.session) {
         req.session.destroy();
         res.clearCookie('session-id');
